1ba4b085341763f8f934d548b479cbb39f9604d5
israel2x <ezurita@fiec.espol.edu.ec>
2025-06-25 19:02:09 -0500

    Inicio app paises y uso de navigator - 25/06/2025

---
+++ README.md
@@ -0,0 +1,3 @@
+# paises_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/paises_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.paises_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
